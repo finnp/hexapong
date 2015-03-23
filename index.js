75bd5f67ba471a4d680818d1701d687667ac4ebe
Finn <derfinn@gmail.com>
2015-03-23 22:48:48 +0000

    score, proper wall collisoin

--- index.js
+++ index.js
@@ -3,12 +3,24 @@ var Game = require('crtrdg-gameloop')
 var Arrows = require('crtrdg-arrows')
 var Vec2 = require('vec2')
 var insidePolygon = require('point-in-polygon')
+var Color = require('color')
 
 var radius = 150
 
 function Player() {
     this.width = 4
     this.height = 40
+    this.active = false
+    this.color = Color('#ff0000')
+    this.id = 1
+    this.score = 0
+}
+Player.prototype.collide = function() {
+    // ball
+    players.forEach(function(player) {
+        player.active = true
+    })
+    this.active = false
 }
 Player.prototype.update = function() {
   if(this.arrows.isDown('left')) this.angle += 2
@@ -25,28 +37,33 @@ Player.prototype.draw = function(c) {
     c.translate(this.x + (player.width/2), this.y + (player.height/2))
     c.rotate(deg2rad(this.angle))
     c.rect(- (this.width/2), - (this.height/2),  this.width , this.height)
-    c.fillStyle = '#000'
+    var color = this.color.clone()
+    if(!this.active) color.alpha(0.25)
+    c.fillStyle = color.rgbString()
     c.fill()
     c.restore()
     c.closePath()
+    c.fillStyle = this.color.rgbString()
+    c.font = '48px serif'
+    c.fillText(this.score, 400, 150 + this.id * 100)
 }
 
-var player = new Player()
-player.arrows = new Arrows()
-player.angle = 0 // degree
+function Ball() {
+    this.reset()
+    this.radius = 2
 
-var player2 = new Player()
-player2.arrows = new Arrows()
-player2.arrows.useWASD()
-player2.angle = 180
-
-function Ball() {}
+}
+Ball.prototype.reset = function() {
+    this.x = 200
+    this.y = 200
+    this.direction = Math.random()*360
+}
 Ball.prototype.update = function() {
     var p = getCircleCoordinates(0, 0, 1, this.direction)
     this.x += p.x 
     this.y += p.y
 }
-Ball.prototype.checkCollision = function (player) {
+Ball.prototype.checkPlayerCollision = function (player) {
     var cx = this.x 
     var cy = this.y
     
@@ -72,14 +89,49 @@ Ball.prototype.checkCollision = function (player) {
       return point.toArray()  
     })
     
-    if(insidePolygon(ballVector.toArray(), playerVertices)) {
+    if(player.active && insidePolygon(ballVector.toArray(), playerVertices)) {
+        player.collide()
         resetBallPosition()
         this.direction = 180 - this.direction + 2* player.angle
         this.direction = this.direction % 360
     }
+}
+
+Ball.prototype.checkWallCollision = function (){
+    
+    var cx = this.x 
+    var cy = this.y
+    var angle =  3
+    
+    var ballVector = Vec2([cx, cy]).add( Vec2([this.radius, 0]).rotate(this.direction * (Math.PI / 180)) )
+    
     if(!insidePolygon(ballVector.toArray(), hexagon)) {
         resetBallPosition()
-        this.direction = 180 + this.direction
+        
+        if(insidePolygon(ballVector.toArray(), [hexagon[0],  [hexagon[0][0],hexagon[1][1]], hexagon[1]])){
+            angle = 30
+        }
+        if(insidePolygon(ballVector.toArray(), [hexagon[1],  [hexagon[1][0],hexagon[1][1]+10], [hexagon[2][0],hexagon[2][1]+10], hexagon[2]])){
+            angle = 90
+        }
+        if(insidePolygon(ballVector.toArray(), [hexagon[2],  [hexagon[3][0],hexagon[2][1]], hexagon[3]])){
+            angle = 150
+        }
+        if(insidePolygon(ballVector.toArray(), [hexagon[3],  [hexagon[3][0],hexagon[4][1]], hexagon[4]])){
+            angle = 210
+        }
+        if(insidePolygon(ballVector.toArray(), [hexagon[4],  [hexagon[4][0],hexagon[4][1]-10], [hexagon[5][0],hexagon[5][1]-10], hexagon[5]])){
+            angle = 270
+        }
+        if(insidePolygon(ballVector.toArray(), [hexagon[5],  [hexagon[0][0],hexagon[5][1]], hexagon[0]])){
+            angle = 330
+        }
+        
+        players.forEach(function(player) {
+            if(!player.active) player.score++
+        })
+        
+        this.direction = 180 - this.direction + 2*angle
         this.direction = this.direction % 360
     }
 }
@@ -95,16 +147,29 @@ Ball.prototype.draw = function(c) {
 }
 
 var ball = new Ball()
-ball.x = 200
-ball.y = 200
-ball.radius = 2
-ball.direction = 0
 
 var game = new Game({width: 800, height: 600})
 var polygonRadius = 200
 var hexagon = createHexagon(200, 200, polygonRadius)
 
+var player = new Player()
+player.arrows = new Arrows()
+player.angle = 0 // degree
 
+var player2 = new Player()
+player2.id = 2
+player2.arrows = new Arrows()
+player2.arrows.useWASD()
+player2.angle = 180
+player2.color = Color('#0000ff')
+
+var players = [player, player2]
+
+function init() {
+    ball.reset()
+    players[Math.round(Math.random())].active = true
+}
+init()
 
 function createHexagon(x, y, r) {
     var points = []
@@ -131,13 +196,20 @@ function getCircleCoordinates(x, y, r, deg) {
   return q
 }
 
+function drawPolygon(c, polygon) {
+    c.moveTo(polygon[0][0], polygon[0][1]) 
+    for(var i = 1; i < polygon.length; i++) c.lineTo(polygon[i][0], polygon[i][1]) 
+    c.lineTo(polygon[0][0], polygon[0][1])
+}
+
 game.on('update', function() {
 // player
     player.update()
     player2.update()
 // ball
-   ball.checkCollision(player)
-   ball.checkCollision(player2)
+   ball.checkPlayerCollision(player)
+   ball.checkPlayerCollision(player2)
+   ball.checkWallCollision() 
    ball.update()
 })
 
@@ -148,7 +220,7 @@ game.on('draw', function(c) {
     c.fillStyle = '#fff'
     c.fill()
     c.lineWidth = 2
-    c.strokeStyle = '#ff0000'
+    c.strokeStyle = '#aaa'
     c.stroke()
     c.closePath()
 
@@ -158,9 +230,7 @@ game.on('draw', function(c) {
 
     // hexagon
     c.beginPath()
-    c.moveTo(hexagon[0][0], hexagon[0][1]) 
-    for(var i = 1; i < hexagon.length; i++) c.lineTo(hexagon[i][0], hexagon[i][1]) 
-    c.lineTo(hexagon[0][0], hexagon[0][1])
+    drawPolygon(c, hexagon)
     c.strokeStyle = '#000'
     c.stroke()
     c.closePath()
